aab4d4b5f0677cb95ddb6a7684e2b4a95baa0cfc
dimasrizkyhp <dimas.rizky.hp@gmail.com>
2021-01-20 19:06:35 +0700

    add build stage

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -6,4 +6,8 @@ class Config {
     def default_docker_jenkins_tool = 'docker'
 
     def default_golang_base_image = 'tobapramudia/tkpd-demo:onbuild'
+
+    def default_docker_registry_jenkins_cred = 'cred-docker'
+
+    def default_docker_registry = "https://registry-1.docker.io/v2/"
 }
