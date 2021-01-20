9c92e01d6f6563b485ba01d0e0350e8204ebe117
dimasrizkyhp <dimas.rizky.hp@gmail.com>
2021-01-20 18:41:02 +0700

    add prebuild stage

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -3,4 +3,7 @@ package com.workshop
 
 class Config {
     // Place Default Variable Here
+    def default_docker_jenkins_tool = 'docker'
+
+    def default_golang_base_image = 'tobapramudia/tkpd-demo:onbuild'
 }
